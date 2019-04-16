70fe7cc065265a6efdf46b3f71e612d8ff0026ca
Andreyfr <andrey.levko90@gmail.com>
2019-04-16 22:34:22 +0300

    Update auth_req.py

--- auth_req.py
+++ auth_req.py
@@ -14,3 +14,19 @@ with requests.Session() as s:
     r = requests.get(url_auth)
     print (r.status_code)
     print (r.headers)
+
+    
+#Using disable_warnings (InsecureRequestWarning) will disable any output from the script when trying to log in to sites 
+#with unverified SSL certificates.
+import requests
+from requests.packages.urllib3.exceptions 
+import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+payload = {
+    'inUserName': 'USERNAME',
+    'inUserPass': 'PASSWD'
+}
+url = 'https://itunesconnect.apple.com/login'
+r = requests.post(url, data=payload, verify=False)
+
+print(r.status_code)
